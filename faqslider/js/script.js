54e92b5615bcee45ccb93d41c5f28147c307dc22
Bao Khanh Tran <tranthibaokhanh@gmail.com>
2015-06-11 14:54:44 -0500

    FAQ

---
+++ faqslider/js/script.js
@@ -0,0 +1,16 @@
+// Accordian
+var action ="click";
+var speed = 500;
+
+$(function() {
+	$('li.q').on(action, function() {
+		// Question handler
+		// Get the next element           Select all other answers
+		$(this).next().slideToggle(speed).siblings('li.a').slideUp();
+		// Get image for active question
+		var img = $(this).children('img');
+		// Remove the 'rotate' class for ll images excepthe active
+		$('img').not(img).removeClass('rotate');
+		img.toggleClass('rotate');
+	})
+})
